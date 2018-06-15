f8c7e1ebbff6ca1d196e82a8b35d7794ba77caab
nahum sonck <33633504+nahumcoding@users.noreply.github.com>
2018-06-15 19:20:00 -0400

    worked on server.js npm package express, body-parser and path git commit -m

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//Express Configuration
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+//Setup for Body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Setup for local CSS Use
+app.use(express.static("public"));
+
+//Routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//Server Listener
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+})
